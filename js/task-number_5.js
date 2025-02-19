51f4f003fbfa078fb1dc0d22396507e3605f7666
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-02-19 20:24:32 +0200

    make hw

--- js/task-number_5.js
+++ js/task-number_5.js
@@ -84,7 +84,9 @@ class Car {
   decelerate(value) {
     if (this.speed > 0) {
       this.speed -= value;
+      console.log(this.speed);
     } else {
+      console.log("not all right");
       return false;
     }
   }
@@ -105,21 +107,20 @@ class Car {
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-// Car.getSpecs(mustang);
 mustang.turnOn();
 mustang.accelerate(50);
-// mustang.drive(2);
+mustang.drive(2);
 
 // Car.getSpecs(mustang);
 // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
 
-// mustang.decelerate(20);
-// mustang.drive(1);
-// mustang.turnOff();
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
 
 // Car.getSpecs(mustang);
 // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
 
-// console.log(mustang.price); // 2000
-// mustang.price = 4000;
-// console.log(mustang.price); // 4000
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
